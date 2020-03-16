978f73bdabf3ddf3bd2c41808b69130037182cf6
manish-dalal <manish.dalal@daffodilsw.com>
2020-03-16 18:26:10 +0530

    Updates

---
+++ precache-manifest.6a1b27d3af427c6d4646b64d76c56214.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa93a5dab9b998d9e1ea089c0c7069f",
+    "url": "/url-shortener/index.html"
+  },
+  {
+    "revision": "a517e39f0d57eb3c223f",
+    "url": "/url-shortener/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2dad1cadb6845fcd1465",
+    "url": "/url-shortener/static/css/main.ccb1ce65.chunk.css"
+  },
+  {
+    "revision": "a517e39f0d57eb3c223f",
+    "url": "/url-shortener/static/js/2.8ea551b6.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/url-shortener/static/js/2.8ea551b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dad1cadb6845fcd1465",
+    "url": "/url-shortener/static/js/main.f41c53d3.chunk.js"
+  },
+  {
+    "revision": "42ec6fee60971a057db1",
+    "url": "/url-shortener/static/js/runtime-main.b44e1244.js"
+  },
+  {
+    "revision": "79d44bd45be48ae7daa601b9f74534e8",
+    "url": "/url-shortener/static/media/logo.79d44bd4.png"
+  }
+]);
